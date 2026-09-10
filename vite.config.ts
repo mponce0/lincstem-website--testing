10a372c0ae4451b6b33bd0265d09236c6862d5f6
Marcelo Ponce <m.ponce@utoronto.ca>
2026-09-09 22:08:27 -0400

    fixing the error with JPG via vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+  base: "/lincstem-website--testing/",
+  assetsInclude: ["**/*.JPG"],
+
   server: {
     port: 5173,
   },
